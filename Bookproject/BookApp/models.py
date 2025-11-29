321884bb90590092aa3ec20368a1f901d590b6b5
Madhiyoli <madhiyoli3307@gmail.com>
2025-11-29 10:09:43 +0530

    Initial commit

---
+++ Bookproject/BookApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib import admin
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200,help_text="Enter the book title")
+    author = models.CharField(max_length=100,help_text="Enter the author's name")
+    published_date = models.DateField()
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
